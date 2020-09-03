0920348595fe587b6daeeb2e855eb435c300e218
DannyAllegrezza <danny@dannyallegrezza.com>
2020-09-03 19:32:32 -0400

    task: Setup tsconfig.json file

--- multimath/app/app.ts
+++ multimath/app/app.ts
@@ -1,7 +1,7 @@
 function startGame() {
     var messagesEl = document.getElementById('messages');
-    messagesEl.innerText = 'Welcome to MultiMatch! Starting a new game....';
+    messagesEl!.innerText = 'Welcome to MultiMatch! Starting a new game....';
 }
 
 // wire things up
-document.getElementById('startGame').addEventListener('click', startGame);
+document.getElementById('startGame')!.addEventListener('click', startGame);
--- multimath/js/app.js
+++ multimath/js/app.js
@@ -1,6 +1,7 @@
+"use strict";
 function startGame() {
     var messagesEl = document.getElementById('messages');
     messagesEl.innerText = 'Welcome to MultiMatch! Starting a new game....';
 }
-// wire things up
 document.getElementById('startGame').addEventListener('click', startGame);
+//# sourceMappingURL=app.js.map
