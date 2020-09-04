bc6b7d7705e45b1bc8cfda1c9b4b16606ef224b6
DannyAllegrezza <danny@dannyallegrezza.com>
2020-09-04 13:55:02 -0400

    task: function type demo

--- multimath/app/app.ts
+++ multimath/app/app.ts
@@ -2,7 +2,7 @@ function startGame() {
     let playerName: string | undefined = getInputValue('playername');
     logPlayer(playerName);
 
-    postScore(100, playerName);
+    postScore(-5, playerName);
 }
 
 function logPlayer(name: string = 'MultiMath Player'): void {
@@ -18,13 +18,25 @@ function getInputValue(elementID: string): string | undefined {
 }
 
 function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+    let logger: (value: string) => void;
+    if (score < 0) {
+        logger = logError;
+    } else {
+        logger = logMessage;
+    }
+
     const scoreElement: HTMLElement | null = document.getElementById('postedScores');
     scoreElement!.innerText = `${score} : ${playerName}`;
+
+    logger(`Score: ${score}`);
 }
 
+// notice how logMessage and logError have the same signature..
 const logMessage = (message: string) => console.log(message);
+
+function logError(err: string): void {
+    console.error(err);
+}
 
 // wire things up
 document.getElementById('startGame')!.addEventListener('click', startGame);
-
-logMessage("Welcome to MultiMath");
